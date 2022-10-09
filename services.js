d9d4c34bbe1f66fb882b038484e9dfaea1ca6b38
emmnesty <elyjhonatan@hotmail.com>
2022-10-09 20:03:35 -0300

    Adicionando logs de sistema

--- services.js
+++ services.js
@@ -1,15 +1,20 @@
 const dao = require('./conexao_mocados');
+const logger = require('./logger/logger');
 
 
 function getEventos(req, res) {
+    logger.info('INICIANDO BUSCA POR TODOS OS EVENTOS')
     try {
         dao.selectAll(res);
+        logger.info('CHAMADA DOS EVENTOS EXECUTADA NORMALMENTE')
     } catch (error) {
-        console.error('erro')
+        logger.error("Erro")
     }
 }
 
+
 function getEventoPorId(req, res) {
+    logger.info('INICIANDO BUSCA POR EVENTOS ID')
     id = parseInt(req.params.id);
     if (req.params.id) dao.selectWhereId(id, res);
 }
